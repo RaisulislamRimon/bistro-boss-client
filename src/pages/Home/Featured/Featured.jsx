0cf952fd154708341570bf8d75100fa1f9b5d986
rimonppip0 <rimonppip0@gmail.com>
2024-09-15 10:56:24 +0000

    add featured section

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,39 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+
+export const Featured = () => {
+  // Get the current date and time
+  const date = new Date();
+
+  // Format the date using the Intl.DateTimeFormat API
+  const formattedDate = date.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  // Display the formatted date
+  // console.log(formattedDate); // Output: Today's date (e.g., "September 15, 2024")
+  return (
+    <div className="featured-item">
+      <SectionTitle heading="featured item" subHeading="check it out" />
+      <div className="md:flex justify-center items-center py-8 px-16">
+        <div>
+          <img src={featuredImg} alt="featuredImg" className="" />
+        </div>
+        <div className="md:ml-10">
+          {/* <p>20 October, 2024</p> */}
+          <p>{formattedDate}</p>
+          <p className="uppercase">Where can i get some?</p>
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+            adipisci dolor voluptates nulla quidem in eligendi omnis nam sint
+            tenetur molestias autem, porro eos incidunt. Dolorem reiciendis
+            repellat ut consequuntur.
+          </h3>
+          <button className="btn btn-outline">Order Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
