d7d4a31c9f4f050c9347588cf881a9b6874319b1
rimonppip0 <rimonppip0@gmail.com>
2024-09-16 13:28:57 +0000

    creating testimonials section

---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+export const Testimonials = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"What Our Clients Say"}
+        heading={"Testimonials"}
+      />
+    </section>
+  );
+};
