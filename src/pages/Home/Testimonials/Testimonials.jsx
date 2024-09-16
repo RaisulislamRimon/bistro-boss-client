26afaddfabfd41244c904e4e46be04d561d4d828
rimonppip0 <rimonppip0@gmail.com>
2024-09-16 14:17:48 +0000

    showing reviews

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -1,12 +1,42 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
 
 export const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setReviews(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(reviews);
   return (
-    <section>
+    <section className="my-20">
       <SectionTitle
         subHeading={"What Our Clients Say"}
         heading={"Testimonials"}
       />
+      <div>
+        {reviews.length}
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {/* <SwiperSlide>Slide 1</SwiperSlide> */}
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div>
+                <p>Review details{reviews?.details}</p>
+                <p className="text-yellow-600">Rating:{reviews?.rating}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 };
